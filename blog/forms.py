4849ab903f377231502e20a8fa7aadb6415dfef9
Aleksei Anisimov <real79@yandex.ru>
2020-05-02 05:33:48 +0700

    Initial commit

--- blog/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import ArticleModel
-
-
-class CreateArticleForm(forms.ModelForm):
-    class Meta:
-        model = ArticleModel
-        fields = ['title', 'body', 'image']
